52bd0bf5648acd3ee439879921bea62050ec5c9c
Taowd <taowd@outlook.om>
2023-09-16 21:05:22 +0800

    学习代码生成-优化逻辑

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.file_search import FileSearchTool
 if __name__ == "__main__":
     directory = r"E:\GitHub\test"  # 替换为您要搜索的目录路径
     keyword = "npm"  # 替换为您要搜索的关键字
-    file_types = (".txt", ".md")  # 替换为您要搜索的文件类型的元组，例如 (".txt", ".doc")
+    file_types = (".txt", ".md", ".js")  # 替换为您要搜索的文件类型的元组，例如 (".txt", ".doc")
 
     file_search_tool = FileSearchTool(directory)
     search_results = file_search_tool.search_files(keyword, file_types)
